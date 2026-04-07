4aa7b04965a4634dab79a0a0bdcddb49639c5921
daciel936-gif <daciel936@gmail.com>
2026-04-07 09:20:48 -0500

    Update 2. Estudiantes aprobados.py

--- 2. Estudiantes aprobados.py
+++ 2. Estudiantes aprobados.py
@@ -1,6 +1,6 @@
 def aprobados(estudiantes):
     return [nombre for nombre, nota in estudiantes if nota >= 3.0]
 
-# Ejemplo
+
 datos = [("Ana", 3.5), ("Luis", 2.9), ("Pedro", 4.0)]
 print(aprobados(datos))
