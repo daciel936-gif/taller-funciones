aa61a4611738c2efe7ca24bb99ccd4776193bcaf
daciel936-gif <daciel936@gmail.com>
2026-04-07 09:20:27 -0500

    Update 1. Promedio.py

--- 1. Promedio.py
+++ 1. Promedio.py
@@ -2,6 +2,6 @@ def analizar_notas(notas):
     promedio = sum(notas) / len(notas)
     return promedio, max(notas), min(notas)
 
-# Ejemplo
+
 notas = [3.5, 4.2, 2.8, 5.0]
 print(analizar_notas(notas))
